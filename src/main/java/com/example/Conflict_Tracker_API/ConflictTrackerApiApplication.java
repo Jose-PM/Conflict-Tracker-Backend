9582ebf349f09ddf947d08f3b699c758bbd1e4a1
Jose-PM <jpadilla@elpuig.xeill.net>
2026-04-28 16:45:39 +0200

    cambios CorsConfig

--- src/main/java/com/example/Conflict_Tracker_API/ConflictTrackerApiApplication.java
+++ src/main/java/com/example/Conflict_Tracker_API/ConflictTrackerApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConflictTrackerApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ConflictTrackerApiApplication.class, args);
 	}
